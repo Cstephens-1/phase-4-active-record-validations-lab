733ed05434c42ad40972cad4048d0d301b7769f8
Cory <corystephens012@gmail.com>
2021-09-21 13:54:07 -0400

    commit tree e234d62cd4bbf7d503a1315db1b884f6776b7ab6

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,14 @@
 class Post < ApplicationRecord
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    # validates :category {"Fiction" || "Non-Fiction"}
+    validates :wrong_title
+
+    def wrong_title
+        if title == "True Facts"
+            errors.add(:title, "Title not allowed")
+        end
+    end
 end
